a8244a6071423c76a1f0b74ecb10c1c77aff338c
Md. Nazrul Islam Khan <nazrulkhan.dk@gmail.com>
2022-12-14 19:47:06 +0600

    data flow diagram applied

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Variant, Product, ProductVariant, ProductImage, ProductVariantPrice
+
 
 # Register your models here.
+@admin.register(Variant)
+class VariantAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created_at", "updated_at")
+    list_per_page = 15
+    list_filter = ("title", "created_at", "updated_at")
+    search_fields = ("id", "title")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "sku", "created_at", "updated_at")
+    list_per_page = 15
+    list_filter = ("title", "sku", "created_at", "updated_at")
+    search_fields = ("id", "title", "sku")
+
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = ("id", "variant_title", "variant", "product", "created_at", "updated_at")
+    list_per_page = 15
+    list_filter = ("variant_title", "variant", "product", "created_at", "updated_at")
+    search_fields = ("id", "variant_title", "variant", "product",)
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "product", "thumbnail", "created_at", "updated_at")
+    list_per_page = 15
+    list_filter = ("product", "thumbnail", "created_at", "updated_at")
+    search_fields = ("id", "thumbnail", "product",)
+
+
+@admin.register(ProductVariantPrice)
+class ProductVariantPriceAdmin(admin.ModelAdmin):
+    list_display = ("id", "product_variant_one", "product_variant_two", "product_variant_three", "product", "price", "stock")
+    list_per_page = 15
+    list_filter = ("product", "price", "stock")
+    search_fields = ("id", "product_variant_one", "product_variant_two", "product_variant_three", "product", "price", "stock")
--- src/product/models.py
+++ src/product/models.py
@@ -1,38 +1,130 @@
 from django.db import models
 from config.g_model import TimeStampMixin
+from django.core.exceptions import ValidationError
 
 
 # Create your models here.
 class Variant(TimeStampMixin):
-    title = models.CharField(max_length=40, unique=True)
-    description = models.TextField()
-    active = models.BooleanField(default=True)
+    title = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        if self.title:
+            return f"{self.title}"
+        else:
+            return "NO_TITLE"
+    
+    def clean(self) -> None:
+        if not self.title:
+            raise ValidationError("Title cannot be null or empty")
+        return super().clean()
+
+    def save(self, *args, **kwargs) -> None:
+        self.clean()
+        return super(Variant, self).save(*args, **kwargs)
 
 
 class Product(TimeStampMixin):
-    title = models.CharField(max_length=255)
-    sku = models.SlugField(max_length=255, unique=True)
-    description = models.TextField()
+    title = models.CharField(max_length=255, null=True, blank=True)
+    sku = models.SlugField(max_length=255, unique=True, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
-
-class ProductImage(TimeStampMixin):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    file_path = models.URLField()
+    def __str__(self) -> str:
+        if self.title and self.sku:
+            return f"{self.title} - {self.sku}"
+        elif self.title and not self.sku:
+            return f"{self.sku} - NO_SKU"
+        elif not self.title and self.sku:
+            return f"NO_TITLE - {self.sku}"
+        else:
+            return "NO_TITLE - NO_SKU"
+    
+    def clean(self) -> None:
+        if not self.title:
+            raise ValidationError("Title cannot be null or empty")
+        if not self.sku:
+            raise ValidationError("sku cannot be null or empty")
+        return super().clean()
+    
+    def save(self, *args, **kwargs) -> None:
+        self.clean()
+        return super(Product, self).save(*args, **kwargs)
 
 
 class ProductVariant(TimeStampMixin):
-    variant_title = models.CharField(max_length=255)
-    variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variant_title = models.CharField(max_length=255, null=True, blank=True)
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self) -> str:
+        if self.variant_title:
+            return f"{self.variant_title}"
+        else:
+            return "NO_VARIENT_TITLE"
+    
+    def clean(self) -> None:
+        if not self.variant_title:
+            raise ValidationError("variant_title cannot be null or empty")
+        if not self.variant:
+            raise ValidationError("varient relation cannot be empty")
+        if not self.product:
+            raise ValidationError("product relation cannot be empty")
+        return super().clean()
+
+    def save(self, *args, **kwargs) -> None:
+        self.clean()
+        return super(ProductVariant, self).save(*args, **kwargs)
+
+
+class ProductImage(TimeStampMixin):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    file_path = models.URLField(max_length=255, null=True, blank=True)
+    thumbnail = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        if self.product:
+            return f"{self.product.title}"
+        else:
+            return "NO_PRODUCT_IMAGE_NAME"
+    
+    def clean(self) -> None:
+        if not self.file_path:
+            raise ValidationError("file_path cannot be null or empty")
+        if not self.thumbnail:
+            raise ValidationError("thumbnail cannot be null or empty")
+        if not self.product:
+            raise ValidationError("product relation cannot be empty")
+        return super().clean()
+
+    def save(self, *args, **kwargs) -> None:
+        self.clean()
+        return super(ProductImage, self).save(*args, **kwargs)
 
 
 class ProductVariantPrice(TimeStampMixin):
-    product_variant_one = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
-                                            related_name='product_variant_one')
-    product_variant_two = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
-                                            related_name='product_variant_two')
-    product_variant_three = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
-                                              related_name='product_variant_three')
-    price = models.FloatField()
-    stock = models.FloatField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_variant_one = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True, related_name='product_variant_one')
+    product_variant_two = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True, related_name='product_variant_two')
+    product_variant_three = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True, related_name='product_variant_three')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    price = models.FloatField(default=0.0, null=True, blank=True)
+    stock = models.PositiveIntegerField(default=0, null=True, blank=True)
+
+    def __str__(self) -> str:
+        if self.product and self.price:
+            return f"{self.product} - {self.price}"
+        else:
+            return "NO_PRODUCT_VARIENT_PRICE"
+    
+    def clean(self) -> None:
+        if not self.price:
+            raise ValidationError("price cannot be null or empty")
+        if not self.stock:
+            raise ValidationError("stock cannot be null or empty")
+        if not self.product:
+            raise ValidationError("product relation cannot be empty")
+        return super().clean()
+
+    def save(self, *args, **kwargs) -> None:
+        self.clean()
+        return super(ProductVariantPrice, self).save(*args, **kwargs)
+

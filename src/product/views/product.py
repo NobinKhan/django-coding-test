54c10804e174dedb6a062e00165cafa0bf92c8ab
Md. Nazrul Islam Khan <nazrulkhan.dk@gmail.com>
2022-12-15 09:21:11 +0600

    data filter - 1 and 2: Done

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView, ListView
-
+from django.db.models import Q
 from product.models import Variant, Product
 
 
@@ -16,14 +16,32 @@ class CreateProductView(TemplateView):
 
 class ProductView(ListView):
     template_name = 'products/list.html'
-    # context_object_name = 'data'
     paginate_by = 2
 
     def get_queryset(self):
-        print("\n\n triger \n\n")
-        return Product.objects.all().prefetch_related('ProductVariantPrice')
+        productTitle = self.request.GET.get('title')
+        productvariant = self.request.GET.get('variant')
+        price_from = self.request.GET.get('price_from')
+        price_to = self.request.GET.get('price_to')
+        date = self.request.GET.get('date')
 
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductView, self).get_context_data(*args, **kwargs)
-    #     context['products'] = self.queryset
-    #     return context
+        qs = Product.objects.prefetch_related('ProductVariantPrice')
+        if productTitle:
+            qs = qs.filter(Q(title__contains=productTitle))
+        if productvariant:
+            qs = qs.filter(
+                Q(ProductVariantPrice__product_variant_one__variant_title=productvariant) |
+                Q(ProductVariantPrice__product_variant_two__variant_title=productvariant) |
+                Q(ProductVariantPrice__product_variant_three__variant_title=productvariant)
+            )
+        if price_from and price_to:
+            qs = qs.filter(Q(ProductVariantPrice__price__range=(price_from, price_to)))
+        if date:
+            qs = qs.filter(Q(created_at__contains=date) | Q(updated_at__contains=date))
+        return qs.distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['varients'] = Variant.objects.prefetch_related('productvariant_set').distinct()
+        return context
+
